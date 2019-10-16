47b798d931c27d269e31f5b84b91dc080102ba93
Ilya Voronin <voroninilya115@gmail.com>
2019-10-16 09:41:10 +0300

    add some methods and classes to HuffmanCoding

--- include/HuffmanCoding.h
+++ include/HuffmanCoding.h
@@ -6,6 +6,22 @@
 #include <vector>
 
 class HuffmanCoding {
+private:
+    class Tree {
+    private:
+        class Node {
+        public:
+            bool isLeaf;
+            Symbol symb;
+            Node *left, *right;
+            Node();
+        };
+
+        Node *root;
+        Tree();
+    };
+
+    Tree buildTree(std::vector <Symbol> &data);
 public:
     void enocde(std::vector <Symbol> &data);
     void decode(std::vector <Symbol> &data);
--- src/HuffmanCoding.cpp
+++ src/HuffmanCoding.cpp
@@ -1,5 +1,19 @@
 #include "HuffmanCoding.h"
 
+HuffmanCoding::Tree::Node::Node() {
+    isLeaf = false;
+    left = nullptr;
+    right = nullptr;
+}
+
+HuffmanCoding::Tree::Tree() {
+    root = new Node;
+}
+
+HuffmanCoding::Tree HuffmanCoding::buildTree(std::vector<Symbol> &data) {
+
+}
+
 void HuffmanCoding::enocde(std::vector<Symbol> &data) {
 
 }
