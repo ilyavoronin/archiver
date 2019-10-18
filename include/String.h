018b4b69d75feee9cb0a788c2847e7a03f44621d
Ilya Voronin <voroninilya115@gmail.com>
2019-10-18 23:14:49 +0300

    add String class

---
+++ include/String.h
@@ -0,0 +1,20 @@
+#ifndef ARCHIVER_STRING_H
+#define ARCHIVER_STRING_H
+
+#include <vector>
+
+template <typename T>
+class String {
+private:
+    std::vector <T> str;
+public:
+    String& add(T symb);
+    String& operator+=(String &ot);
+    String operator+(String &ot);
+    String operator+(T symb);
+    String& operator+=(T symb);
+    T& operator[](int i);
+    int size();
+};
+
+#endif //ARCHIVER_STRING_H
---
+++ src/String.cpp
@@ -0,0 +1,43 @@
+#include "String.h"
+
+template <typename T>
+String<T>& String<T>::add(T symb) {
+    str.push_back(symb);
+    return *this;
+}
+
+template <typename T>
+String<T>& String<T>::operator+=(String &ot) {
+    this->str.insert(this->str.begin(), ot.str.begin(), ot.str.end());
+    return *this;
+}
+
+template <typename T>
+String<T> String<T>::operator+(String &ot) {
+    String<T> tmp = *this;
+    tmp += ot;
+    return tmp;
+}
+
+template <typename T>
+String<T>& String<T>::operator+=(T symb) {
+    add(symb);
+    return *this;
+}
+
+template <typename T>
+String<T> String<T>::operator+(T symb) {
+    String<T> tmp = *this;
+    tmp.add(symb);
+    return tmp;
+}
+
+template <typename T>
+T& String<T>::operator[](int i) {
+    return str[i];
+}
+
+template <typename T>
+int String<T>::size() {
+    return str.size();
+}
