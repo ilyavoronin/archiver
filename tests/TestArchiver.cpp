0e769744a8ff8341506fb9a3f5ce4373b66e12a5
Ilya Voronin <voroninilya115@gmail.com>
2019-11-09 19:04:36 +0300

    add tests for Archiver

--- tests/TestArchiver.cpp
+++ tests/TestArchiver.cpp
@@ -1,4 +1,39 @@
-//
-// Created by ILYA on 09.11.2019.
-//
+#include "gtest/gtest.h"
 
+#include <Archiver.h>
+#include <fstream>
+
+class ArchiverTests : public ::testing::Test {
+protected:
+    std::string inFilename = "in.tmp";
+    std::string arcFileName = "arc.tmp";
+    std::string outFileName = "out.tmp";
+    Archiver arc;
+    const int TEST_SIZE = 5e6;
+    void SetUp() override {
+        std::ofstream out(inFilename, std::ios::binary);
+        for (int i = 0; i < TEST_SIZE; i++) {
+            out << char(rand() % 256);
+        }
+        out.close();
+    };
+
+    bool cmpFiles(std::string file1, std::string file2) {
+        std::ifstream in1(file1, std::ios::binary);
+        std::ifstream in2(file2, std::ios::binary);
+        while (!in1.eof()) {
+            char c1 = in1.get();
+            char c2 = in2.get();
+            if (c1 != c2) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+TEST_F(ArchiverTests, testZipUnzip) {
+    arc.zip(inFilename, arcFileName);
+    arc.unzip(arcFileName, outFileName);
+    ASSERT_TRUE(cmpFiles(inFilename, outFileName));
+}
