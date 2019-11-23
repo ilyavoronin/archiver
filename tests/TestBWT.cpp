72555ee3be26dc14d72d43a722d8163c718c4e49
Ilya Voronin <voroninilya115@gmail.com>
2019-11-23 11:49:59 +0300

    add tests for BWT

--- tests/TestBWT.cpp
+++ tests/TestBWT.cpp
@@ -1,4 +1,38 @@
-//
-// Created by ILYA on 23.11.2019.
-//
+#include "gtest/gtest.h"
+#include "BWT.h"
 
+TEST(BWTTests, testEncode) {
+    std::vector <char> vdata = {'a', 'b', 'a', 'c', 'a', 'b', 'a', char(255)};
+    DataInfo dataInfo;
+    String <Symbol> data;
+    for (auto z : vdata) {
+        data.add(z);
+    }
+
+    std::vector <char> expected = {char(255), 'c', 'b', 'b', 'a', 'a', 'a', 'a'};
+    BWT().encode(data, dataInfo);
+
+    for (int i = 0; i < 8; i++) {
+        ASSERT_EQ(data[i], expected[i]);
+    }
+    int firstSuffixNumber;
+    dataInfo.read(firstSuffixNumber);
+    ASSERT_EQ(firstSuffixNumber, 0);
+}
+
+TEST(BWTTests, testDecode) {
+    std::vector <char> vdata = {char(255), 'c', 'b', 'b', 'a', 'a', 'a', 'a'};
+    DataInfo dataInfo;
+    dataInfo.write((int)0);
+    String <Symbol> data;
+    for (auto z : vdata) {
+        data.add(z);
+    }
+
+    std::vector <char> expected = {'a', 'b', 'a', 'c', 'a', 'b', 'a', char(255)};
+    BWT().decode(data, dataInfo);
+
+    for (int i = 0; i < 8; i++) {
+        ASSERT_EQ(data[i], expected[i]);
+    }
+}
